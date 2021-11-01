078617058dbb4fa2feaeeb3591e66abbee3970fa
VlNadezhda <valkovanadezhda17@gmail.com>
2021-11-01 17:31:10 +0300

    +YCrCb

--- main.cpp
+++ main.cpp
@@ -19,22 +19,27 @@ int main(int argc, char** argv)
 
     cv::Mat im_gr;
     cvtColor(im_rgb,im_gr,cv::COLOR_RGB2GRAY);
-    imshow("gray", im_gr); // Show our image inside the created window.
+    imshow("gray", im_gr);
     cv::waitKey(0);
 
     cv::Mat im_canny;
     cv::Canny(im_rgb,im_canny,100,200);
-    imshow("Canny", im_canny); // Show our image inside the created window.
+    imshow("Canny", im_canny);
     cv::waitKey(0);
 
     cv::Mat im_gauss;
     GaussianBlur(im_rgb,im_gauss, cv::Size(5, 5), 0);
-    imshow("Gaussian", im_gauss); // Show our image inside the created window.
+    imshow("Gaussian", im_gauss);
     cv::waitKey(0);
 
     cv::Mat im_hsv;
     cvtColor(im_rgb,im_hsv, cv::COLOR_BGR2HSV);
-    imshow("HSV", im_hsv); // Show our image inside the created window.
+    imshow("HSV", im_hsv);
+    cv::waitKey(0);
+
+    cv::Mat im_clr;
+    cvtColor(im_rgb,im_clr, cv::COLOR_BGR2YCrCb);
+    imshow("YCrCb", im_clr);
     cv::waitKey(0);
     return 0;
 }
